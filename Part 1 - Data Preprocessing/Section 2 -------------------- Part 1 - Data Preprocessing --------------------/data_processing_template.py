2348ebd236231486ac20542033905ad365860f93
Sriramkumar Jothimani <sriramjothimani@gmail.com>
2018-05-26 15:22:52 +0530

    Add the folder setup for MI A-Z

---
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_processing_template.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#I Importing the dataset
+dataset = pd.read_csv('Data.csv')
