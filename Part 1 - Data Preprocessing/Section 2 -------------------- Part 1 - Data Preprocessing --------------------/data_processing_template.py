ccac8ed429b3503d603fff9cb47be46bf41277b4
Sriramkumar Jothimani <sriramjothimani@gmail.com>
2018-05-27 19:28:54 +0530

    Completing Data preprocessing template

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_processing_template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_processing_template.py
@@ -7,5 +7,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#I Importing the dataset
+# Importing the dataset
 dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:, 3].values
+
+# Splitting the dataset into the traning set and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
